660f1564b683cd4a9bded0473c2617ba917091c2
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-12 21:30:44 +0300

    Updated home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Navbar from '@/components/Navbar'
 import Billboard from '@/components/Billboard'
 import MovieList from '@/components/MovieList'
 import useMovieList from '@/hooks/useMovieList'
+import useFavorites from '@/hooks/useFavorites'
+import InfoModal from '@/components/InfoModal'
 
 
 
@@ -27,13 +29,16 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMovieList()
+  const { data: favorites = [] } = useFavorites()
 
   return (
       <>
+        <InfoModal visible onClose={() => {}} />
         <Navbar />  
         <Billboard />
         <div className='pb-40'>
             <MovieList title='Trending Now' data={movies} />
+            <MovieList title='My List' data={favorites} />
         </div>
       </>
   )
