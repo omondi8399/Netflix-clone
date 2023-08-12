2005b5ad7fdd90720526422b1376afde548eeebe
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-12 21:31:07 +0300

    Updated favorite api

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -24,11 +24,49 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         const user = await prismadb.user.update({
             where: {
-                email: currentUser.email
+                email: currentUser.email || '',
+            },
+            data: {
+                favoriteIds: {
+                    push: movieId,
+                }
+            }
+        })
+        return res.status(200).json(user)
+    } 
+
+    if (req.method === 'DELETE') {
+        const { currentUser } = await serverAuth(req)
+
+        const { movieId } = req.body
+
+        const existingMovie = await prismadb.movie.findUnique({
+            where: {
+                id: movieId,
             }
         })
 
-    } catch (error) {
+        if (!existingMovie) {
+            throw new Error('Invalid ID')
+        }
+
+        const updatedFavoriteIds = without(currentUser.favoriteIds, movieId)
+
+        const updatedUser = await prismadb.user.update({
+            where: {
+                email: currentUser.email || '',
+            },
+            data: {
+                favoriteIds: updatedFavoriteIds,
+            }
+        })
+
+        return res.status(200).json(updatedUser)
+    }
+
+    return res.status(405).end()
+    
+    catch (error) {
         console.log(error){
             return res.status(400).end()
         }
