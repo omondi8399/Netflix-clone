4787ee870e659f33ed3616ed3a99802a0bb8ac1a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-13 10:14:12 +0300

    Updated Moviecard component

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import { BsFillPlayFill } from 'react-icons/bs'
+import { BiChevronDown } from 'react-icons/bi'
 import FavoriteButton from './FavoriteButton'
 import { useRouter } from 'next/router'
+import useInfoModal from '@/hooks/useInfoModal'
 
 interface MovieCardProps {
     data: Record<string, any>
@@ -12,6 +14,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
     const router = useRouter()
+    const { openModal } = useInfoModal()
 
 return (
     <div className='group bg-zinc-900 col-span relative h-[12vw]'>
@@ -84,6 +87,12 @@ return (
                             <BsFillPlayFill size={30} />
                         </div>
                         <FavoriteButton movieId={data?.id} />
+                        <div  
+                            onClick={() => openModal(data?.id)}
+                            className='cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white
+                        border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300'>
+                            <BiChevronDown size={30} className='text-white group-hover/item:text-neutral-300' />
+                        </div>
                     </div>
 
                     <p className='text-green-400 font-semibold mt-4'>
