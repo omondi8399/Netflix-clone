26d85df673b6e10991848f098d166b3cb03557c7
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-13 13:59:58 +0300

    Updated AccountMenu component

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,7 +1,7 @@
 import { signOut } from 'next-auth/react';
 import React from 'react';
 
-import useCurrentUser from '@/hooks/useCurrentUser';
+import useCurrentUser from '../hooks/useCurrentUser';
 
 interface AccountMenuProps {
   visible?: boolean;
