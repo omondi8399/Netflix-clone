5672f5bd769de26e47064c12e5e83d40ae1fae1f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-13 10:17:50 +0300

    Updated AccountMenu component

--- components/AccountMenu.jsx
+++ components/AccountMenu.jsx
@@ -1,3 +1,4 @@
+import useCurrentUser from '@/hooks/useCurrentUser'
 import { signOut } from 'next-auth/react'
 import React from 'react'
 
@@ -6,6 +7,8 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+    const { data } = useCurrentUser()
+
     if (!visible) {
         return null
     }
@@ -16,7 +19,7 @@ return (
             <div className='px-3 group/item flex flex-row gap-3 items-center w-full'>
                 <img className='w-8 rounded-md' src='/images/default-blue.png' alt='' />
                 <p className='text-white text-sm group-hover/items:underline'>
-                    Username
+                    {data?.name}
                 </p>
             </div>
             <hr className='bg-gray-600 border-0 h-px my-4' />
