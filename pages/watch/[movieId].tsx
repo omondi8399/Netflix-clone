eb1fbfa1df1493bf423fb331362b0fbda691953b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-13 14:37:41 +0300

    Updated watch page

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { useRouter } from 'next/router';
 import useMovie from '@/hooks/useMovie';
 
@@ -12,7 +12,7 @@ const Watch = () => {
   return (
     <div className="h-screen w-screen bg-black">
       <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
-        <ArrowLeftIcon onClick={() => router.push('/')} className="w-4 md:w-10 text-white cursor-pointer hover:opacity-80 transition" />
+        <AiOutlineArrowLeft onClick={() => router.push('/')} className="w-4 md:w-10 text-white cursor-pointer hover:opacity-80 transition" />
         <p className="text-white text-1xl md:text-3xl font-bold">
           <span className="font-light">Watching:</span> {data?.title}
         </p>
